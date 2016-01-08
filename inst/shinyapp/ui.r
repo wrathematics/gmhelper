4b730e9f7c579071903233940f55c0ee88099edf
wrathematics <wrathematics@gmail.com>
2016-01-08 01:47:07 -0500

    styling, about page

--- inst/shinyapp/ui.r
+++ inst/shinyapp/ui.r
@@ -2,10 +2,18 @@ library(shiny)
 
 
 about.thisapp <- HTML("
-  <p>
-  This is a collection of rollers for use with fantasy tabletop
-  gaming useful to game/dungeon masters.
+  <h1>GM Helper</h1>
   
+  <h3>Random Rollers for Lazy Assholes</h3>
+  
+  <p style='font-size:120%;'>
+  This is a collection of generators for use with fantasy tabletop
+  gaming useful to game/dungeon masters.  When you're too lazy to
+  even roll a die and look up the result in a table, there's
+  the GM Helper.
+  </p>
+  
+  <p style='font-size:120%;'>
   The generators are written in R, and the web framework is 
   written in shiny.  The source code for each is available
   here 
@@ -17,10 +25,10 @@ about.thisapp <- HTML("
 
 
 shinyUI(
-  fluidPage(
-    tags$head(
-      tags$link(rel="stylesheet", type="text/css", href="superhero.min.css")
-    ),
+  fluidPage(theme="superhero.min.css",
+    # tags$head(
+    #   tags$link(rel="stylesheet", type="text/css", href="superhero.min.css")
+    # ),
     
     navbarPage(
       title="GM Helper", 
