37281f1935dc1ede52715eba428eab2d7bdbe5ae
Drew Schmidt <wrathematics@gmail.com>
2018-03-14 23:12:31 -0400

    add henchmen

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,7 @@
 .onLoad = function(libname, pkgname)
 {
   data("gmh_directions", package="gmhelper", envir=parent.env(environment()))
+  data("gmh_henchmen", package="gmhelper", envir=parent.env(environment()))
   data("gmh_racenames", package="gmhelper", envir=parent.env(environment()))
   data("gmh_services", package="gmhelper", envir=parent.env(environment()))
   
--- inst/shinyapp/pages/tables.r
+++ inst/shinyapp/pages/tables.r
@@ -12,7 +12,8 @@ opts = list(
 output$main_tables <- renderUI({
   mainPanel(
     tabsetPanel(
-      tabPanel("Services", uiOutput("tables_services_ui"))
+      tabPanel("Services", uiOutput("tables_services_ui")),
+      tabPanel("Henchmen", uiOutput("tables_henchmen_ui"))
     )
   )
 })
@@ -20,13 +21,14 @@ output$main_tables <- renderUI({
 
 
 services = gmhelper:::gmh_services
-tables_services = c("Construction", "Inn", "Livestock", "Lodgings", "Menu")
 
 output$tables_services_ui = renderUI({
   list(
     sidebarLayout(
       sidebarPanel(
-        radioButtons(inputId="tables_services_radio", label="Services", tables_services, inline=FALSE)
+        radioButtons(inputId="tables_services_radio", label="Services",
+        c("Construction", "Inn", "Livestock", "Lodgings", "Menu"),
+        inline=FALSE)
       ),
       
       mainPanel(
@@ -37,6 +39,33 @@ output$tables_services_ui = renderUI({
             Livestock=services$livestock,
             Lodgings=services$lodgings,
             Menu=services$menu
+          )
+        })
+      )
+    )
+  )
+})
+
+
+
+henchmen = gmhelper:::gmh_henchmen
+
+output$tables_henchmen_ui = renderUI({
+  list(
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons(inputId="tables_henchmen_radio", label="Services",
+        c("Experts", "Hirelings", "Mercenaries", "Shipmen"),
+        inline=FALSE)
+      ),
+      
+      mainPanel(
+        renderDataTable(options=opts, {
+          switch(input$tables_henchmen_radio,
+            Experts=henchmen$experts,
+            Hirelings=henchmen$hirelings,
+            Mercenaries=henchmen$mercenaries,
+            Shipmen=henchmen$shipmen
           )
         })
       )
